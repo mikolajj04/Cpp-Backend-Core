4a0adeb63d0c4a865dc87002a692f27774cce7a1
mikolajj04 <Mikolajjussak@gmail.com>
2026-01-07 23:38:04 +0100

    refactor: extract rendering logic to a separate module
    Moved Draw function from main.cpp to Draw.cpp for better code organization.

---
+++ SnakeGame/SnakeGame/SnakeGame/Draw.cpp
@@ -0,0 +1,44 @@
+#include "Draw.h"
+
+void Draw(const GameConfig& cfg, SnakeList& snake, int fruitX, int fruitY, int score) {
+    COORD cursorPosition;
+    cursorPosition.X = 0;
+    cursorPosition.Y = 0;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+    //upper wall
+    for (int i = 0; i < cfg.width + 2; i++) { std::cout << "#"; }
+    std::cout << "\n";
+
+
+    //left wall->checking if snake is on j,i position-> right wall
+    for (int i = 0; i < cfg.height; i++) {
+        for (int j = 0; j < cfg.width; j++) {
+            if (j == 0) std::cout << "#";
+            if (snake.isSnakeAt(j, i)) {
+                std::cout << "O";
+            }
+            else if (j == fruitX && i == fruitY)
+            {
+                std::cout << "Q";
+            }
+            else {
+                std::cout << " ";
+            }
+            if (j == cfg.width - 1) std::cout << "#";
+        }
+        std::cout << "\n";
+    }
+    //bottom wall
+    for (int i = 0; i < cfg.width + 2; i++) {
+        std::cout << "#";
+    }
+    std::cout << "\n";
+    for (int i = 0; i < cfg.width + 2; i++) {
+        std::cout << "-";
+    }
+    std::cout << "\n";
+    std::cout << "Score: " << score << "| map config: " << cfg.width << "x" << cfg.height << "\n";
+
+
+
+}
---
+++ SnakeGame/SnakeGame/SnakeGame/Draw.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "GameConfig.h"
+#include "SnakeList.h"
+
+void Draw(const GameConfig& cfg, SnakeList& snake, int fruitX, int fruitY, int score);
