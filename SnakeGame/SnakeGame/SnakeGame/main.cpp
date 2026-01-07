25a49c745b2c4f53d03cc3ff77e03053835d270d
mikolajj04 <Mikolajjussak@gmail.com>
2026-01-07 23:58:20 +0100

    feat: integrate SnakeList with gameplay logic in main function.

--- SnakeGame/SnakeGame/SnakeGame/main.cpp
+++ SnakeGame/SnakeGame/SnakeGame/main.cpp
@@ -1,9 +1,15 @@
 #include "SnakeList.h"
 #include "GameConfig.h"
-
+#include "Draw.h"
 
 int main() {
+    GameConfig cfg = loadConfig("config.txt");
+    enum eDirection {stop= 0, left, right, up, down};
+    eDirection dir = stop;
+    SnakeList snake;
 
-
+    int score = 0;
+    int headX = cfg.width / 2;
+    int headY = cfg.height / 2;
     return 0;
 }
