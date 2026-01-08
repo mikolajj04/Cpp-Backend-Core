934e0dadaebe6dd85cb22cd4a9b3b99c6f459c07
mikolajj04 <Mikolajjussak@gmail.com>
2026-01-08 23:10:09 +0100

    add: user input listener
    logic: implement direction switching

--- SnakeGame/SnakeGame/SnakeGame/main.cpp
+++ SnakeGame/SnakeGame/SnakeGame/main.cpp
@@ -4,12 +4,40 @@
 
 int main() {
     GameConfig cfg = loadConfig("config.txt");
-    enum eDirection {stop= 0, left, right, up, down};
-    eDirection dir = stop;
+    enum eDirection {STOP= 0, LEFT, RIGHT, UP, DOWN};
+    eDirection dir = STOP;
     SnakeList snake;
 
     int score = 0;
     int headX = cfg.width / 2;
     int headY = cfg.height / 2;
+    int fruitX = rand() % cfg.width;
+    int fruitY = rand() % cfg.height;
+    bool gameOver = false;
+    snake.push_front(headX,headY);
+
+    while (!gameOver) {
+        if (_kbhit()) {
+            switch (_getch())
+            {
+            case 'a':
+                dir = LEFT;
+                break;
+            case 'd':
+                dir = RIGHT;
+                break;
+            case 'w':
+                dir = UP;
+                break;
+            case 's':
+                dir = DOWN;
+                break;
+            case 'x':
+                gameOver = true;
+                break;
+            }
+        }
+    }
+
     return 0;
 }
