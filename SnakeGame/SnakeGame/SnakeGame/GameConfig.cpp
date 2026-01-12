ad8677359d32f81911c2102c3f55c464835255b7
mikolajj04 <mikolajjussak@gmail.com>
2026-01-12 12:34:10 +0100

    feat(config): Add simple config file validation.

--- SnakeGame/SnakeGame/SnakeGame/GameConfig.cpp
+++ SnakeGame/SnakeGame/SnakeGame/GameConfig.cpp
@@ -5,11 +5,17 @@ GameConfig loadConfig(std::string filename){
 
 	if (!file.is_open()) {
 		std::cout << "Error: cannot open the file: " << filename << "!" << std::endl;
-		std::cout << "Loading default settings..." << std::endl; \
+		std::cout << "Loading default settings..." << std::endl; 
 			return { 20,15,100,false };
 	}
 
 	file >> config.width >> config.height >> config.speed >> config.IsHardcore;
+	if (config.width > 100 || config.height > 24) {
+		std::cout << "							Error: maximum width is 100 and maximum height is 24 "<< std::endl;
+		std::cout << "							Loading default settings..." << std::endl; 
+		
+			return { 20,15,100,false };
+	}
 	file.close();
 	return config;
 
