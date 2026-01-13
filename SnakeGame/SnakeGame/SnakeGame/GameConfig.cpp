ffbab5a2567e840c72583c45d9f2b5b7aae8175d
mikolajj04 <Mikolajjussak@gmail.com>
2026-01-13 20:03:03 +0100

    fix: enhance config validation with user acknowledgement
    Implemented a pause (_getch) to ensure users read configuration error messages.
    
    Added screen clearing after error acknowledgment to maintain UI consistency.

--- SnakeGame/SnakeGame/SnakeGame/GameConfig.cpp
+++ SnakeGame/SnakeGame/SnakeGame/GameConfig.cpp
@@ -1,19 +1,26 @@
 #include "GameConfig.h"
+#include <conio.h>
 GameConfig loadConfig(std::string filename){
 	std::ifstream file(filename);
 	GameConfig config;
 	std::string tempDiff;
 
 	if (!file.is_open()) {
-		std::cout << "Error: cannot open the file: " << filename << "!" << std::endl;
-		std::cout << "Loading default settings..." << std::endl; 
+		std::cerr << "Error: cannot open the file: " << filename << "!" << std::endl;
+		std::cerr << "Loading default settings..." << std::endl; 
+		std::cerr << "Press any key to continue..." << std::endl;
+		_getch();
+		system("cls");
 			return { 20,15,100,Difficulty::EASY };
 	}
 
 	file >> config.width >> config.height >> config.speed  >> tempDiff;
 	if (config.width > 100 || config.height > 24) {
-		std::cout << "							Error: maximum width is 100 and maximum height is 24 "<< std::endl;
-		std::cout << "							Loading default settings..." << std::endl; 
+		std::cerr << "Error: maximum width is 100 and maximum height is 24 "<< std::endl;
+		std::cerr << "Loading default settings..." << std::endl; 
+		std::cerr << "Press any key to continue..." << std::endl;
+		_getch();
+		system("cls");
 		
 			return { 20,15,100,Difficulty::EASY };
 	}
